f1a602855c803d67a6c0fda29374a749d0c8482a
Samuel Askeli <samuel.askeli@aalto.fi>
2020-02-09 01:21:55 +0200

    not working

---
+++ client/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './js/components/App.js'
+
+ReactDom.render(<App/>, document.getElementById('App'))
+//const react = require('react');
+//const dom = require('react-dom');
+//const app = require('./js/components/App.js');
+//
+//dom.ReactDom.render(<App/>, document.getElementById('App'));
