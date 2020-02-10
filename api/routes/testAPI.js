7ecef1ce454cf873acf46c0961b5176493abe30a
Samuel Askeli <samuel.askeli@aalto.fi>
2020-02-10 11:29:10 +0200

    dockerize api

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -1,23 +1,14 @@
-var typeorm = require('typeorm')
-var express = require("express")
+var express = require('express')
 var router = express.Router()
 router.get("/", function(req, res, next) {
     res.send("API is working properly.")
 })
+
 var pgp = require('pg-promise')(/* options */)
+
 require('dotenv').config()
 
-
-const connection = {
-    host: process.env.pghost,
-    port: process.env.pgport,
-    database: process.env.pgdatabase,
-    user: process.env.pguser,
-    password: process.env.pgpassword
-}
-
-
-var db = pgp(connection)
+var db = pgp(process.env.DATABASE_URL)
 db.one('SELECT $1 AS value', 123)
   .then(function (data) {
     console.log('DATA:', data.value)
